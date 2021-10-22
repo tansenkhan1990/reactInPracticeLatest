1d88db0ee4cca63983da7469ffb28512f1b99d21
tansen <tansenkhan1990@gmail.com>
2021-10-22 13:29:41 +0200

    add custom component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+     <h1>pokoemon poko</h1>
+     <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem(){
+    return ( <h3>this is from expense</h3>)
+}
+
+export default ExpenseItem;
